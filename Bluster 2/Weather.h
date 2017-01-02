45418be64e31ab6c5acf0b4a859b628df1413485
Max von Hippel <maxvonhippel1996@gmail.com>
2017-01-02 16:28:05 -0700

    first commit

---
+++ Bluster 2/Weather.h
@@ -0,0 +1,13 @@
+//
+//  Weather.h
+//  Bluster 2
+//
+//  Created by Max von Hippel on 1/2/17.
+//  Copyright © 2017 Max von Hippel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Weather : NSObject
+
+@end
---
+++ Bluster 2/Weather.m
@@ -0,0 +1,13 @@
+//
+//  Weather.m
+//  Bluster 2
+//
+//  Created by Max von Hippel on 1/2/17.
+//  Copyright © 2017 Max von Hippel. All rights reserved.
+//
+
+#import "Weather.h"
+
+@implementation Weather
+
+@end
